96b9d55c12f4e4fb5d0b39ceb423ff56803ea473
Rouke Broersma <mobrockers@gmail.com>
2021-04-19 23:24:06 +0200

    Using $ID instead of $USER

--- build.sh
+++ build.sh
@@ -3,8 +3,8 @@ mkdir -p dist
 sudo chown "$USER:$USER" dist
 export JEKYLL_VERSION=3.8
 docker run --rm \
-  --env JEKYLL_UID=$USER \
-  --env JEKYLL_GID=$USER \
+  --env JEKYLL_UID=$UID \
+  --env JEKYLL_GID=$GID \
   --volume="$PWD/dist:/srv/jekyll/dist" \
   --volume="$PWD/src:/srv/jekyll/src" \
   jekyll/minimal:$JEKYLL_VERSION \
