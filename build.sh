03e0e97065b497d72b8e1f08ab239096449b9d85
Rouke Broersma <mobrockers@gmail.com>
2021-04-19 22:41:31 +0200

    Use builder container non-interactive

--- build.sh
+++ build.sh
@@ -3,5 +3,5 @@ export JEKYLL_VERSION=3.8
 docker run --rm \
   --volume="$PWD/dist:/srv/jekyll/dist" \
   --volume="$PWD/src:/srv/jekyll/src" \
-  -it jekyll/minimal:$JEKYLL_VERSION \
+  jekyll/minimal:$JEKYLL_VERSION \
   jekyll build -s /srv/jekyll/src -d /srv/jekyll/dist
