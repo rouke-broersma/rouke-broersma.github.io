e60d3b3ff496dca70536ad91b8ec1206e3bca86f
Rouke Broersma <mobrockers@gmail.com>
2021-05-07 19:45:21 +0200

    Set working dir for hugo shell script

--- shell.sh
+++ shell.sh
@@ -4,7 +4,7 @@ export HUGO_VERSION=0.82.0
 docker run --rm -it \
   --volume="$(pwd):/project" \
   --publish 1313:1313 \
+  --workdir="/project/src" \
   klakegg/hugo:$HUGO_VERSION-ext-alpine \
-  shell \
-  --environment development
+  shell
 sudo chown -R "$USER:$USER" src
