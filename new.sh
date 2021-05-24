5608f159358b92bf626de26d628650623af288c5
Rouke Broersma <mobrockers@gmail.com>
2021-05-24 12:56:22 +0200

    Support creating draft/scratch posts

--- new.sh
+++ new.sh
@@ -1,20 +1,29 @@
 #!/bin/bash
-title=$1
+if [ "$1" == "draft" ]; then
+  title=$2
+  title=${title,,}
+  title=${title// /-}
+  title="draft/$title.md"
+else
+  title=$1
 
-if [ -z "$title" ]; then
-  echo "Argument missing: Post title"
-  exit 1
+  if [ -z "$title" ]; then
+    echo "Argument missing: Post title"
+    exit 1
+  fi
+
+  title=${title,,}
+  title=${title// /-}
+  year=$(date +'%Y')
+  month=$(date +'%m')
+  day=$(date +'%d')
+
+  title="post/$year/$month/$day/$title.md"
 fi
-
-year=$(date +'%Y')
-month=$(date +'%m')
-day=$(date +'%d')
-
-title="$year/$month/$day/$title.md"
 
 docker run --rm \
   --volume="$(pwd):/project" \
   --workdir="/project/src" \
   klakegg/hugo:ext-alpine \
-  new --kind post "post/$title"
+  new "$title"
 sudo chown -R "$USER:$USER" src
