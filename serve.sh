1a04b5f6843ad5b3a642784d6abb58f4dcf8501c
Rouke Broersma <mobrockers@gmail.com>
2023-02-24 21:11:29 +0100

    Rework build scripts so chown is no longer needed

--- serve.sh
+++ serve.sh
@@ -1,11 +1,16 @@
 #!/bin/bash
-export HUGO_VERSION=0.107.0
 xdg-open http://localhost:1313
+
+source common.sh
+source update-modules.sh
+
 docker run --rm \
+  --user 1000:1000 \
+  --volume="$(pwd)/cache:/cache" \
+  --volume="$(pwd)/dist:/dist" \
   --volume="$(pwd):/src" \
   --publish 1313:1313 \
+  --env-file hugo-environment-variables.env \
   klakegg/hugo:$HUGO_VERSION-ext-alpine \
   server \
-  --source "/src/src" \
-  --environment development
-sudo chown -R "$USER:$USER" src
+  --source "/src/src"
